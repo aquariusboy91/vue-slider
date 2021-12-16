c6983e13985b38f30d3c6f743f803bed91e9d9bd
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-12-16 15:03:03 +0100

    add images

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: {
+             images: 'images/image1.jpg' ,
+             images: 'images/image2.jpg' ,
+             images: 'images/image3.jpg' ,
+             images: 'images/image4.jpg' 
+        },
+        methods: {
+            imgChange: function () {
+              for(i = 0; i < items.length; i++) {
+               
+              }
+            }
+        
+    }
+    
+  })
